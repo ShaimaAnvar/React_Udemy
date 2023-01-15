9bb4dd85904ed3a212e6ee6471ba8071fbb2b1a7
Shaima Anvar <shaima.anvar@gmail.com>
2023-01-14 20:41:09 -0500

    passind data via props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [{
+    date: new Date("2022-07-06"),
+    title:'Insurance',
+    amount:290
+  }, {
+    date:new Date("2022-03-06"),
+    title:'Rent',
+    amount:490
+  }, {
+    date: new Date("2022-05-05"),
+    title:'Loan',
+    amount:590
+  }]
+    ;
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+
+      
     </div>
   );
 }
