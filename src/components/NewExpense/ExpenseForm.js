4a2d33547a19ffee973ecca4d636e66c0be5a4fb
Shaima Anvar <shaima.anvar@gmail.com>
2023-06-29 20:47:28 -0400

    "creating a shared handler function"

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,20 +26,33 @@ const [userInput, setUserInput]=useState({
         setUserInput({...userInput,
             enteredDate:event.target.value});
     }
+
+    const inputChangeHandler =(identifier,value)=>{
+        if(identifier ==="title"){
+            seEnteredTitle(value);
+        }else if(identifier==="date"){
+            setEnteredDate(value);
+        }else{
+            setEnteredAmout(value);
+        };
+    };
     
     return <form>
         <div className="new-expense__controls">
             <div className="new-expense__control label">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                {/* <input type="text" onChange={titleChangeHandler}/> */}
+                <input type="text" onChange={(event)=>inputChangeHandler('title',event.target.value)}/>
             </div>
             <div className="new-expense__control label">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                {/* <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/> */}
+                <input type="text" onChange={(event)=>inputChangeHandler('title',event.target.value)}/>
             </div>
             <div className="new-expense__control label">
                 <label>Date</label>
-                <input type="Date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                {/* <input type="Date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/> */}
+                <input type="text" onChange={(event)=>inputChangeHandler('title',event.target.value)}/>
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
